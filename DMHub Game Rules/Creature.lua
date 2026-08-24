5fd3fc38cfcfd5129b5de2100250b3ff1dfe9333
Tim Clark <github@thc1967.com>
2026-08-23 22:02:27 -0400

    When taking a respite & calculating level up, before transforming victories to xp, first calculate the floor xp that we must have for our level and if we don't have enough, set it to that, then add xp from victories.

--- DMHub Game Rules/Creature.lua
+++ DMHub Game Rules/Creature.lua
@@ -10589,10 +10589,19 @@ function creature:Rest(restType, keepOngoingEffects)
 		local victories = self:try_get("victories", 0)
 		if victories > 0 then
 			local curXp = self:try_get("xp", 0)
-			local newXp = curXp + victories
 			local level = self:CharacterLevel()
 			local xpPerLevel = toint(dmhub.GetSettingValue("xpperlevel") or 16)
 			if xpPerLevel == 0 then xpPerLevel = 16 end
+
+			--A level set directly (builder, import, DM edit) can leave xp below what
+			--that level implies. Pull xp up to the bottom of the current level's band
+			--first, so victories are awarded from the correct baseline.
+			local xpFloorForLevel = (level - 1) * xpPerLevel
+			if curXp < xpFloorForLevel then
+				curXp = xpFloorForLevel
+			end
+
+			local newXp = curXp + victories
 			local xpNextLevel = level * xpPerLevel
 			local newLevel = level
 			local levelingUp = newXp >= xpNextLevel
